08e30645ef12208f8bf31372a1211f099384253e
Crowdbotics <team@crowdbotics.com>
2020-09-21 14:38:53 +0000

    Update models

--- backend/bisalu_mentor_match_20503/wsgi.py
+++ backend/bisalu_mentor_match_20503/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bisalu_mentor_match_20503.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bisalu_mentor_match_20503.settings")
 
 application = get_wsgi_application()
